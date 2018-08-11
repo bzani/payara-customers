7a986e4f1bbe18eaa55708aadb9b98a635bb7ff8
bzani <ubzani@gmail.com>
2018-08-11 17:06:13 -0300

    Using Jackson json de/serializer with JAX-RS

--- acme-customers-api/src/main/java/com/acme/customers/api/rest/v1/RestApplication.java
+++ acme-customers-api/src/main/java/com/acme/customers/api/rest/v1/RestApplication.java
@@ -1,8 +1,28 @@
 package com.acme.customers.api.rest.v1;
 
+import com.acme.customers.api.rest.v1.providers.JacksonProvider;
+import com.acme.customers.api.rest.v1.resources.CustomerResource;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/v1")
 public class RestApplication extends Application {
+
+    /*
+     * JAX-RS will call this method to get all the classes
+     * that are part of the application and will no longer
+     * discover those automatically
+     */
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(JacksonJsonProvider.class);
+        classes.add(JacksonProvider.class);
+        classes.add(CustomerResource.class);
+        return classes;
+    }
 }
